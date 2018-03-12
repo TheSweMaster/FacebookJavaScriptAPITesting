e45c103d5816d57d8055af6bf10fd0128f87aac9
Jacob Gandemo <theswemaster@gmail.com>
2018-03-12 12:41:53 +0100

    Added Solution

---
+++ FacebookAPITesting/Controllers/FacebookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacebookAPITesting.Controllers
+{
+    public class FacebookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
